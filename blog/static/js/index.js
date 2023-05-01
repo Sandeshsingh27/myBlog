86627f3d27b36d83012ef2445c22aa3cbbfee902
Sandesh Singh <sandeshsingh295@gmail.com>
2023-05-01 15:53:35 +0530

    added constraint for blog title in forms in javascript

--- blog/static/js/index.js
+++ blog/static/js/index.js
@@ -1,5 +1,6 @@
-function titleRequired(){
-    var empty = document.forms["addTaskForm"]["title"].value;
+function titleRequired(param){
+    let formName= param.concat('BlogForm');
+    var empty = document.forms[formName]["title"].value;
     empty= empty.trim();
     if (empty == ""|| empty == null){
     alert("Title field cannot be empty.");
